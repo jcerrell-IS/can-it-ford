3b1d2143ad8c5791f6a48d932f798b5159a49ec5
Josie Cerrell <jcerrell29@students.claremontmckenna.edu>
2026-07-23 02:36:31 -0500

    Add required MIT attribution header to kks32-derived citation files

--- citations/splat_sim(kks32).py
+++ citations/splat_sim(kks32).py
@@ -1,3 +1,21 @@
+# MIT License
+#
+# Copyright (c) 2026 The mpm-engine authors (see AUTHORS.md)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+
 """Gaussian-splat scene simulated with the warpmpm engine, PhysGaussian style.
 
 Loads or synthesizes a 3D Gaussian-splat cloud, fits it into the grid, fills the interior
